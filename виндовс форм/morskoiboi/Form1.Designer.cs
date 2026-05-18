92729b5a52e32baf34cf70544f3f6106c549004f
Afil <daniltwetwe@mail.ru>
2026-05-18 18:48:36 +0300

    Add files via upload

---
+++ виндовс форм/morskoiboi/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace morskoiboi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playerGrid = new System.Windows.Forms.DataGridView();
+            this.computerGrid = new System.Windows.Forms.DataGridView();
+            this.startButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.oneDeckShipButton = new System.Windows.Forms.Button();
+            this.twoDeckShipButton = new System.Windows.Forms.Button();
+            this.threeDeckShipButton = new System.Windows.Forms.Button();
+            this.fourDeckShipButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.playerGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playerGrid
+            // 
+            this.playerGrid.AllowUserToAddRows = false;
+            this.playerGrid.AllowUserToDeleteRows = false;
+            this.playerGrid.AllowUserToResizeColumns = false;
+            this.playerGrid.AllowUserToResizeRows = false;
+            this.playerGrid.BackgroundColor = System.Drawing.SystemColors.MenuHighlight;
+            this.playerGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.playerGrid.Location = new System.Drawing.Point(-6, 70);
+            this.playerGrid.MultiSelect = false;
+            this.playerGrid.Name = "playerGrid";
+            this.playerGrid.RowHeadersVisible = false;
+            this.playerGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.playerGrid.Size = new System.Drawing.Size(240, 150);
+            this.playerGrid.TabIndex = 0;
+            this.playerGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.playerGrid_CellContentClick);
+            // 
+            // computerGrid
+            // 
+            this.computerGrid.AllowUserToAddRows = false;
+            this.computerGrid.AllowUserToDeleteRows = false;
+            this.computerGrid.AllowUserToResizeColumns = false;
+            this.computerGrid.AllowUserToResizeRows = false;
+            this.computerGrid.BackgroundColor = System.Drawing.SystemColors.Highlight;
+            this.computerGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.computerGrid.ColumnHeadersVisible = false;
+            this.computerGrid.Location = new System.Drawing.Point(562, 70);
+            this.computerGrid.Name = "computerGrid";
+            this.computerGrid.RowHeadersVisible = false;
+            this.computerGrid.Size = new System.Drawing.Size(240, 150);
+            this.computerGrid.TabIndex = 1;
+            this.computerGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.computerGrid_CellContentClick);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(324, 314);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(111, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Начать игру";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusLabel.Location = new System.Drawing.Point(356, 9);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(70, 25);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "label1";
+            // 
+            // oneDeckShipButton
+            // 
+            this.oneDeckShipButton.Location = new System.Drawing.Point(13, 313);
+            this.oneDeckShipButton.Name = "oneDeckShipButton";
+            this.oneDeckShipButton.Size = new System.Drawing.Size(75, 23);
+            this.oneDeckShipButton.TabIndex = 4;
+            this.oneDeckShipButton.Text = "1";
+            this.oneDeckShipButton.UseVisualStyleBackColor = true;
+            this.oneDeckShipButton.Click += new System.EventHandler(this.oneDeckShipButton_Click);
+            // 
+            // twoDeckShipButton
+            // 
+            this.twoDeckShipButton.Location = new System.Drawing.Point(167, 313);
+            this.twoDeckShipButton.Name = "twoDeckShipButton";
+            this.twoDeckShipButton.Size = new System.Drawing.Size(75, 23);
+            this.twoDeckShipButton.TabIndex = 5;
+            this.twoDeckShipButton.Text = "2";
+            this.twoDeckShipButton.UseVisualStyleBackColor = true;
+            this.twoDeckShipButton.Click += new System.EventHandler(this.twoDeckShipButton_Click);
+            // 
+            // threeDeckShipButton
+            // 
+            this.threeDeckShipButton.Location = new System.Drawing.Point(13, 385);
+            this.threeDeckShipButton.Name = "threeDeckShipButton";
+            this.threeDeckShipButton.Size = new System.Drawing.Size(75, 23);
+            this.threeDeckShipButton.TabIndex = 6;
+            this.threeDeckShipButton.Text = "3";
+            this.threeDeckShipButton.UseVisualStyleBackColor = true;
+            this.threeDeckShipButton.Click += new System.EventHandler(this.threeDeckShipButton_Click);
+            // 
+            // fourDeckShipButton
+            // 
+            this.fourDeckShipButton.Location = new System.Drawing.Point(167, 384);
+            this.fourDeckShipButton.Name = "fourDeckShipButton";
+            this.fourDeckShipButton.Size = new System.Drawing.Size(75, 23);
+            this.fourDeckShipButton.TabIndex = 7;
+            this.fourDeckShipButton.Text = "4";
+            this.fourDeckShipButton.UseVisualStyleBackColor = true;
+            this.fourDeckShipButton.Click += new System.EventHandler(this.fourDeckShipButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fourDeckShipButton);
+            this.Controls.Add(this.threeDeckShipButton);
+            this.Controls.Add(this.twoDeckShipButton);
+            this.Controls.Add(this.oneDeckShipButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.computerGrid);
+            this.Controls.Add(this.playerGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.playerGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView playerGrid;
+        private System.Windows.Forms.DataGridView computerGrid;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button oneDeckShipButton;
+        private System.Windows.Forms.Button twoDeckShipButton;
+        private System.Windows.Forms.Button threeDeckShipButton;
+        private System.Windows.Forms.Button fourDeckShipButton;
+    }
+}
+
---
+++ виндовс форм/morskoiboi/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace morskoiboi
+{
+    public partial class Form1 : Form
+    {
+        private int[,] playerField = new int[10, 10];
+        private int[,] computerField = new int[10, 10];
+        private Random random = new Random();
+        private bool gameStarted = false;
+        private int playerShipsRemaining = 10;
+        private int computerShipsRemaining = 10;
+        private bool playerTurn = true;
+        private int shipSizeToPlace = 0;
+        private bool shipPlacementHorizontal = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeGrid(playerGrid);
+            InitializeGrid(computerGrid);
+        }
+        private void InitializeGrid(DataGridView grid)
+        {
+            grid.RowCount = 10;
+            grid.ColumnCount = 10;
+            for (int i = 0; i < 10; i++)
+            {
+                grid.Columns[i].Width = 30;
+                grid.Rows[i].Height = 30;
+            }
+            grid.RowHeadersVisible = false;
+            grid.ColumnHeadersVisible = false;
+            grid.AllowUserToAddRows = false;
+            grid.AllowUserToDeleteRows = false;
+            grid.AllowUserToResizeColumns = false;
+            grid.AllowUserToResizeRows = false;
+            grid.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            grid.MultiSelect = false;
+        }
+        private void PlaceComputerShips()
+        {
+            int shipsToPlace = 10;
+            while (shipsToPlace > 0)
+            {
+                int x = random.Next(0, 10);
+                int y = random.Next(0, 10);
+                if (computerField[x, y] == 0)
+
+                    computerField[x, y] = 1;
+                shipsToPlace--;
+            }
+        }
+
+        private void oneDeckShipButton_Click(object sender, EventArgs e)
+        {
+            shipSizeToPlace = 1;
+        }
+        private void twoDeckShipButton_Click(object sender, EventArgs e)
+        {
+            shipSizeToPlace = 2;
+        }
+
+        private void threeDeckShipButton_Click(object sender, EventArgs e)
+        {
+            shipSizeToPlace = 3;
+        }
+
+        private void fourDeckShipButton_Click(object sender, EventArgs e)
+        {
+            shipSizeToPlace = 4;
+        }
+
+
+        private bool CanPlaceShip(int startX, int startY, int size, bool horizontal, int[,] field)
+        {
+
+            if (horizontal)
+            {
+                if (startX + size > 10) return false;
+                for (int i = startX; i < startX + size; i++)
+                    if (field[i, startY] != 0) return false;
+            }
+            else
+            {
+                if (startY + size > 10) return false;
+                for (int i = startY; i < startY + size; i++)
+                    if (field[startX, i] != 0) return false;
+            }
+            return true;
+        }
+        private void PlaceShip(int startX, int startY, int size, bool horizontal,
+         int[,] field, DataGridView grid)
+        {
+            if (horizontal)
+            {
+                for (int i = startX; i < startX + size; i++)
+                {
+                    field[i, startY] = 1;
+                    grid.Rows[startY].Cells[i].Style.BackColor = Color.Gray;
+                }
+            }
+            else
+            {
+                for (int i = startY; i < startY + size; i++)
+                {
+                    field[startX, i] = 1;
+                    grid.Rows[i].Cells[startX].Style.BackColor = Color.Gray;
+                }
+            }
+        }
+
+        private void playerGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (gameStarted) return;
+            if (shipSizeToPlace == 0)
+            {
+                MessageBox.Show("Выберите размер корабля.");
+                return;
+            }
+            int x = e.ColumnIndex;
+            int y = e.RowIndex;
+            if (playerField[x, y] == 0)
+            {
+                if (CanPlaceShip(x, y, shipSizeToPlace, shipPlacementHorizontal,
+                playerField))
+                {
+                    PlaceShip(x, y, shipSizeToPlace, shipPlacementHorizontal,
+                    playerField, playerGrid);
+                    shipSizeToPlace = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Нельзя разместить корабль в этом месте.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Здесь уже есть корабль.");
+            }
+        }
+        private int CountShips(int[,] field)
+        {
+            int count = 0;
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    if (field[i, j] == 1) count++;
+            if (CountShips(playerField) == 5)
+                startButton.Enabled = true;
+            else
+                startButton.Enabled = false;
+            return count;
+            
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (!gameStarted && CountShips(playerField) == 10)
+            {
+                computerField = new int[10, 10];
+                computerShipsRemaining = 10;
+                playerShipsRemaining = 10;
+                for (int i = 0; i < 10; i++)
+                    for (int j = 0; j < 10; j++)
+                        computerGrid.Rows[i].Cells[j].Style.BackColor =
+                        Color.LightBlue;
+                PlaceComputerShips();
+                gameStarted = true;
+                startButton.Enabled = false;
+                playerTurn = true;
+                MessageBox.Show("Игра началась! Ваш ход.");
+            }
+        }
+
+        private void computerGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (!gameStarted || !playerTurn) return;
+            int x = e.ColumnIndex;
+            int y = e.RowIndex;
+            if (computerField[x, y] == 1)
+            {
+                computerField[x, y] = 3;
+                computerGrid.Rows[y].Cells[x].Style.BackColor = Color.Red;
+                computerShipsRemaining--;
+                if (computerShipsRemaining == 0)
+                {
+                    MessageBox.Show("Вы победили!");
+                    EndGame();
+                    return;
+                }
+                MessageBox.Show("Попадание! Ходите еще раз.");
+            }
+            else if (computerField[x, y] == 0)
+            {
+                computerField[x, y] = 2;
+                computerGrid.Rows[y].Cells[x].Style.BackColor = Color.White;
+                playerTurn = false;
+                MessageBox.Show("Промах. Ход компьютера.");
+                ComputerTurn();
+            }
+            else
+            {
+                MessageBox.Show("Вы уже стреляли в эту клетку.");
+            }
+        }
+        private async void ComputerTurn()
+        {
+            await Task.Delay(1000);
+            while (!playerTurn)
+            {
+                int x = random.Next(0, 10);
+                int y = random.Next(0, 10);
+                if (playerField[x, y] == 1)
+                {
+                    playerField[x, y] = 3;
+                    playerGrid.Rows[y].Cells[x].Style.BackColor = Color.Red;
+                    playerShipsRemaining--;
+                    if (playerShipsRemaining == 0)
+                    {
+                        MessageBox.Show("Компьютер победил!");
+                        EndGame();
+                        return;
+                    }
+                    MessageBox.Show("Компьютер попал! Ходит еще раз.");
+                }
+                else if (playerField[x, y] == 0)
+                {
+                    playerField[x, y] = 2;
+                    playerGrid.Rows[y].Cells[x].Style.BackColor = Color.White;
+                    playerTurn = true;
+                    MessageBox.Show("Компьютер промахнулся. Ваш ход.");
+                }
+            }
+        }
+        private void EndGame()
+        {
+            gameStarted = false;
+            startButton.Enabled = true;
+        }
+
+    }
+
+}
+
+    
